7496b68183936375838ae785e28903ff271354cf
ub-victor <victoireushindi371@gmail.com>
2025-06-03 05:10:38 +0200

    Initial commit

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,11 @@
-var character = 'mario';
+var character = 'victor';
 console.log("Hello, ".concat(character, "!"));
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
+var circ = function (diameter) {
+    return Math.PI * diameter;
+};
+console.log(circ(10));
--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,15 @@
-var character = 'mario';
+var character = 'victor';
 console.log(`Hello, ${character}!`);
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
 
+
 inputs.forEach(input => {
   console.log(input);
 });
+
+
+const circ = (diameter: number) => {
+  return Math.PI * diameter;
+};
+console.log(circ(10)); 
