7d387a5c7cf4aebb265d1f9dad88b0ecb2959b38
ub-victor <victoireushindi371@gmail.com>
2025-06-03 02:00:39 +0200

    Initial commit

--- sandbox.js
+++ sandbox.js
@@ -2,3 +2,8 @@ var character = 'mario';
 console.log("Hello, ".concat(character, "!"));
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
+
+
+inputs.forEach(input=> {   
+   console.log(input);
+})
