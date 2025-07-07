a91495127e4f807ce1c37a41d798f249a636e20a
ub-victor <victoireushindi371@gmail.com>
2025-07-07 13:35:03 +0200

    Initial commit

--- sandbox.ts
+++ sandbox.ts
@@ -1,15 +1,28 @@
-var character = 'victor';
-console.log(`Hello, ${character}!`);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
+// var character = 'victor';
+// console.log(`Hello, ${character}!`);
+// var inputs = document.querySelectorAll('input');
+// console.log(inputs);
 
 
-inputs.forEach(input => {
-  console.log(input);
-});
+// inputs.forEach(input => {
+//   console.log(input);
+// });
 
 
-const circ = (diameter: number) => {
-  return Math.PI * diameter;
-};
-console.log(circ(10)); 
+// const circ = (diameter: number) => {
+//   return Math.PI * diameter;
+// };
+// console.log(circ(10)); 
+
+// Arrays 
+let names = ["victoire", "Vincent", "Vainqueur",]
+names.push("Victorine")
+name.push(12);
+
+let mixed = ["ken", 4, 'chun-li']
+
+
+
+
+
+// Onject
