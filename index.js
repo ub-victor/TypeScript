c320d4efeb4e99052d3d4e90338e3f926cb77d8e
ub-victor <victoireushindi371@gmail.com>
2025-11-29 15:42:59 +0200

    intro to pizza app

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const menu = [
     {name: "Hawaiian", price: 10},
     {name: "Veggie", price: 9}
 ]
+
+const cashInRegister = 100;
+const orderQueue = []
+
+/**
+ * Challenge: Add a utility function "addNewPizza" that takes a pizza object
+ * and adds it to the menu.
+ */
+
+function addNewPizza(pizzaObj){
+    menu.push(pizzaObj)
+}
