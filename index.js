b8a1c063b3e80dec394af64166f0ef92e560b4b9
ub-victor <victoireushindi371@gmail.com>
2025-11-29 15:43:19 +0200

    intro to pizza app

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ const orderQueue = []
 function addNewPizza(pizzaObj){
     menu.push(pizzaObj)
 }
+
+/**
+ * Write another utility  function, placeOrder, that takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue 
+ *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. returns the new order object (just in case we need it later)
+ */
