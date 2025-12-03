1fc6f148747ebe41ca743a09152a9d5b35323c97
ub-victor <victoireushindi371@gmail.com>
2025-12-04 00:54:11 +0200

    tyoe Aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -3,5 +3,5 @@ const logDetails = (uid, item) => {
     console.log(`${item} has a uid of ${uid}`);
 };
 const greet = (user, uid) => {
-    console.log(`${item} has a uid of ${uid}`);
+    console.log(`${user.name} says hello`);
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -6,5 +6,5 @@ const logDetails = (uid: String | Number, item: String)=>{
 
 
 const greet = (user: String , uid: String | number)=>{
-    console.log(`${item} has a uid of ${uid}`);
+    console.log(`${user.name} says hello`);
 }
