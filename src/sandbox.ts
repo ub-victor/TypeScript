8c3a9cc75d1e4762001edbd299f89382ca620082
ub-victor <victoireushindi371@gmail.com>
2025-12-04 00:59:14 +0200

    tyoe Aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,10 +1,11 @@
 type StringOrNum = string| number;
+type objWithName = {name: string, uid: StringOrNum};
 
 const logDetails = (uid: StringOrNum, item: String)=>{
     console.log(`${item} has a uid of ${uid}`);
 }
 
 
-const greet = (user: {name: string, uid: StringOrNum})=>{
+const greet = (user: )=>{
     console.log(`${user.name} says hello`);
 }
