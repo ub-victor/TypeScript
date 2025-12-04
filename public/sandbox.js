00e0e21ccbcfeb54eacca038184e152552cea766
ub-victor <victoireushindi371@gmail.com>
2025-12-04 15:25:36 +0200

    Function Signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,11 +1,1 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
-};
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
-};
-const greetAgain = (user) => {
-    console.log(`${user.name} says hello`);
-};
-// next # 10
